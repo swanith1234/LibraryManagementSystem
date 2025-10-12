fd19772cec35e0d9982bd894a7847b4d54a7ed60
swanith1234 <swanithpidugu@gmail.com>
2025-10-12 20:47:23 +0530

    added cors

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'books',
     'users',
     'borrow',
-    'drf_yasg',  # For Swagger documentation
+    'drf_yasg', # For Swagger documentation
+    'corsheaders',
     # 'rest_framework',  # Uncomment when using DRF for APIs
 ]
 # backend/settings.py
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 TEMPLATES = [
     {
